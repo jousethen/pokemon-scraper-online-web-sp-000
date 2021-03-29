b341b000079a4ea073d24526fea49b0dc20875fb
Jouse then <jthen93@gmail.com>
2021-03-29 14:42:18 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,12 +8,12 @@ class Pokemon
     @id = id
   end
   
-  def save
+  def self.save
     sql = <<-SQL
       INSERT INTO students (name, type)
       VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, self.name, self.type)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
 end
