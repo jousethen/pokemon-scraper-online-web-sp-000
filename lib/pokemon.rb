a51b521d0afff7ccee11b95e7b791d7d19080817
Jouse then <jthen93@gmail.com>
2021-03-29 14:39:28 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,10 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db 
+  
+  def initialize (name:, type:, db:, id: nil)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
 end
